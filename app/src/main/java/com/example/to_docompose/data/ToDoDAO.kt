eb803fdb69c7579187f35a49e2ba602cd0387b01
Maxeeepro <maksym.gaidai@gmail.com>
2023-04-17 14:09:31 +0300

    Adding Repository and DAO

--- app/src/main/java/com/example/to_docompose/data/ToDoDAO.kt
+++ app/src/main/java/com/example/to_docompose/data/ToDoDAO.kt
@@ -1,9 +1,6 @@
 package com.example.to_docompose.data
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import com.example.to_docompose.data.models.ToDoTask
 import kotlinx.coroutines.flow.Flow
 
@@ -17,4 +14,22 @@ interface ToDoDAO {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun addTask(toDoTask: ToDoTask)
+
+    @Update
+    suspend fun updateTask(toDoTask: ToDoTask)
+
+    @Delete
+    suspend fun deleteTask(toDoTask: ToDoTask)
+
+    @Query("DELETE FROM DATABASE_TABLE")
+    suspend fun deleteAllTasks()
+
+    @Query("SELECT * FROM DATABASE_TABLE WHERE title LIKE :searchQuery OR description LIKE :searchQuery")
+    fun searchDatabase(searchQuery: String): Flow<List<ToDoTask>>
+
+    @Query("SELECT * FROM DATABASE_TABLE ORDER BY CASE WHEN priority LIKE 'L%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'H%' THEN 3 END")
+    fun sortByLowPriority(): Flow<List<ToDoTask>>
+
+    @Query("SELECT * FROM DATABASE_TABLE ORDER BY CASE WHEN priority LIKE 'H%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'L%' THEN 3 END")
+    fun sortByHighPriority(): Flow<List<ToDoTask>>
 }
---
+++ app/src/main/java/com/example/to_docompose/data/repositories/ToDoRepository.kt
@@ -0,0 +1,37 @@
+package com.example.to_docompose.data.repositories
+
+import com.example.to_docompose.data.ToDoDAO
+import com.example.to_docompose.data.models.ToDoTask
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ToDoRepository @Inject constructor(private val toDoDAO: ToDoDAO) {
+    val getAllTasks: Flow<List<ToDoTask>> = toDoDAO.getAllTasks()
+    val sortByLowPriority: Flow<List<ToDoTask>> = toDoDAO.sortByLowPriority()
+    val sortByHighPriority: Flow<List<ToDoTask>> = toDoDAO.sortByLowPriority()
+
+    fun getSelectedTask(taskId: Int): Flow<ToDoTask> {
+        return toDoDAO.getSelectedTask(taskId = taskId)
+    }
+
+    suspend fun addTask(toDoTask: ToDoTask) {
+        toDoDAO.addTask(toDoTask = toDoTask)
+    }
+
+    suspend fun updateTask(toDoTask: ToDoTask) {
+        toDoDAO.updateTask(toDoTask = toDoTask)
+    }
+
+    suspend fun deleteTask(toDoTask: ToDoTask) {
+        toDoDAO.deleteTask(toDoTask = toDoTask)
+    }
+
+    suspend fun deleteAllTask() {
+        toDoDAO.deleteAllTasks()
+    }
+
+    fun searchDataBase(searchQuery: String) : Flow<List<ToDoTask>>{
+        return toDoDAO.searchDatabase(searchQuery = searchQuery)
+    }
+
+}
