be25ad487defbf0ef139532b069f5a41522b961d
Maxeeepro <maksym.gaidai@gmail.com>
2023-04-25 10:08:24 +0300

    Added Navigation setup and HILT

--- app/src/main/java/com/example/to_docompose/data/repositories/ToDoRepository.kt
+++ app/src/main/java/com/example/to_docompose/data/repositories/ToDoRepository.kt
@@ -2,9 +2,11 @@ package com.example.to_docompose.data.repositories
 
 import com.example.to_docompose.data.ToDoDAO
 import com.example.to_docompose.data.models.ToDoTask
+import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
+@ViewModelScoped //means that instance of the ToDoRepository will be alive as ViewModel
 class ToDoRepository @Inject constructor(private val toDoDAO: ToDoDAO) {
     val getAllTasks: Flow<List<ToDoTask>> = toDoDAO.getAllTasks()
     val sortByLowPriority: Flow<List<ToDoTask>> = toDoDAO.sortByLowPriority()
--- app/src/main/java/com/example/to_docompose/util/Constants.kt
+++ app/src/main/java/com/example/to_docompose/util/Constants.kt
@@ -1,6 +1,16 @@
 package com.example.to_docompose.util
 
 object Constants {
+    //database
     const val DATABASE_TABLE = "todo_table"
     const val DATABASE_NAME = "todo_database"
+
+    //navigation routes
+    //route name/{argument key name}
+    const val LIST_SCREEN = "list/{action}" //action key of the argument
+    const val TASK_SCREEN = "task/{taskId}"
+
+    //names keys arguments
+    const val LIST_ARGUMENT_KEY = "action"
+    const val TASK_ARGUMENT_KEY = "taskId"
 }
